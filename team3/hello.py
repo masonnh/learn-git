9b80de54615036e0294d640bc968809bed3125c2
masonnh <masonlizard@gmail.com>
2022-09-21 13:26:24 -0600

    update hello message

--- team3/hello.py
+++ team3/hello.py
@@ -1,2 +1,7 @@
 print("hello team 3. this is a secret message. share with no one.")
 print("more instructions will follow.")
+print("<*-*>")
+
+# this is a secret comment. only your eyes have seen this.
+# think of all the secrets it contains!
+# wow, that's so cool.
